61814196d20a3f60f24cec6bcbcb932109338b27
Michele Collevati <11272443+Colle11@users.noreply.github.com>
2022-05-09 17:43:13 +0200

    mrc_gpu tests scale as the number of threads per block varies.

--- microsat_MiraCle_tests.sh
+++ microsat_MiraCle_tests.sh
@@ -60,22 +60,118 @@ do
     echo "./build/bin/microsat_MRC_DYN_DLCS_STATS ${i}"
     sleep 10s
     ### MRC_GPU ###
-    ./build/bin/microsat_MRC_GPU_JW_OS_STATS ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_JW_OS_STATS.txt"
-    echo "./build/bin/microsat_MRC_GPU_JW_OS_STATS ${i}"
+    ### 32 TPD ###
+    ./build/bin/microsat_MRC_GPU_JW_OS_STATS_32_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_JW_OS_STATS_32_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_JW_OS_STATS_32_TPB ${i}"
     sleep 10s
-    ./build/bin/microsat_MRC_GPU_JW_TS_STATS ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_JW_TS_STATS.txt"
-    echo "./build/bin/microsat_MRC_GPU_JW_TS_STATS ${i}"
+    ./build/bin/microsat_MRC_GPU_JW_TS_STATS_32_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_JW_TS_STATS_32_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_JW_TS_STATS_32_TPB ${i}"
     sleep 10s
-    ./build/bin/microsat_MRC_GPU_BOHM_STATS ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_BOHM_STATS.txt"
-    echo "./build/bin/microsat_MRC_GPU_BOHM_STATS ${i}"
+    ./build/bin/microsat_MRC_GPU_BOHM_STATS_32_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_BOHM_STATS_32_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_BOHM_STATS_32_TPB ${i}"
     sleep 10s
-    ./build/bin/microsat_MRC_GPU_POSIT_STATS ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_POSIT_STATS.txt"
-    echo "./build/bin/microsat_MRC_GPU_POSIT_STATS ${i}"
+    ./build/bin/microsat_MRC_GPU_POSIT_STATS_32_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_POSIT_STATS_32_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_POSIT_STATS_32_TPB ${i}"
     sleep 10s
-    ./build/bin/microsat_MRC_GPU_DLIS_STATS ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_DLIS_STATS.txt"
-    echo "./build/bin/microsat_MRC_GPU_DLIS_STATS ${i}"
+    ./build/bin/microsat_MRC_GPU_DLIS_STATS_32_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_DLIS_STATS_32_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_DLIS_STATS_32_TPB ${i}"
     sleep 10s
-    ./build/bin/microsat_MRC_GPU_DLCS_STATS ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_DLCS_STATS.txt"
-    echo "./build/bin/microsat_MRC_GPU_DLCS_STATS ${i}"
+    ./build/bin/microsat_MRC_GPU_DLCS_STATS_32_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_DLCS_STATS_32_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_DLCS_STATS_32_TPB ${i}"
+    sleep 10s
+    ### 64 TPD ###
+    ./build/bin/microsat_MRC_GPU_JW_OS_STATS_64_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_JW_OS_STATS_64_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_JW_OS_STATS_64_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_JW_TS_STATS_64_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_JW_TS_STATS_64_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_JW_TS_STATS_64_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_BOHM_STATS_64_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_BOHM_STATS_64_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_BOHM_STATS_64_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_POSIT_STATS_64_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_POSIT_STATS_64_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_POSIT_STATS_64_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_DLIS_STATS_64_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_DLIS_STATS_64_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_DLIS_STATS_64_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_DLCS_STATS_64_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_DLCS_STATS_64_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_DLCS_STATS_64_TPB ${i}"
+    sleep 10s
+    ### 128 TPD ###
+    ./build/bin/microsat_MRC_GPU_JW_OS_STATS_128_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_JW_OS_STATS_128_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_JW_OS_STATS_128_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_JW_TS_STATS_128_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_JW_TS_STATS_128_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_JW_TS_STATS_128_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_BOHM_STATS_128_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_BOHM_STATS_128_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_BOHM_STATS_128_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_POSIT_STATS_128_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_POSIT_STATS_128_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_POSIT_STATS_128_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_DLIS_STATS_128_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_DLIS_STATS_128_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_DLIS_STATS_128_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_DLCS_STATS_128_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_DLCS_STATS_128_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_DLCS_STATS_128_TPB ${i}"
+    sleep 10s
+    ### 256 TPD ###
+    ./build/bin/microsat_MRC_GPU_JW_OS_STATS_256_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_JW_OS_STATS_256_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_JW_OS_STATS_256_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_JW_TS_STATS_256_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_JW_TS_STATS_256_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_JW_TS_STATS_256_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_BOHM_STATS_256_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_BOHM_STATS_256_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_BOHM_STATS_256_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_POSIT_STATS_256_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_POSIT_STATS_256_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_POSIT_STATS_256_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_DLIS_STATS_256_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_DLIS_STATS_256_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_DLIS_STATS_256_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_DLCS_STATS_256_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_DLCS_STATS_256_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_DLCS_STATS_256_TPB ${i}"
+    sleep 10s
+    ### 512 TPD ###
+    ./build/bin/microsat_MRC_GPU_JW_OS_STATS_512_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_JW_OS_STATS_512_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_JW_OS_STATS_512_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_JW_TS_STATS_512_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_JW_TS_STATS_512_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_JW_TS_STATS_512_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_BOHM_STATS_512_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_BOHM_STATS_512_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_BOHM_STATS_512_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_POSIT_STATS_512_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_POSIT_STATS_512_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_POSIT_STATS_512_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_DLIS_STATS_512_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_DLIS_STATS_512_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_DLIS_STATS_512_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_DLCS_STATS_512_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_DLCS_STATS_512_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_DLCS_STATS_512_TPB ${i}"
+    sleep 10s
+    ### 1024 TPD ###
+    ./build/bin/microsat_MRC_GPU_JW_OS_STATS_1024_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_JW_OS_STATS_1024_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_JW_OS_STATS_1024_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_JW_TS_STATS_1024_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_JW_TS_STATS_1024_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_JW_TS_STATS_1024_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_BOHM_STATS_1024_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_BOHM_STATS_1024_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_BOHM_STATS_1024_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_POSIT_STATS_1024_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_POSIT_STATS_1024_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_POSIT_STATS_1024_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_DLIS_STATS_1024_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_DLIS_STATS_1024_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_DLIS_STATS_1024_TPB ${i}"
+    sleep 10s
+    ./build/bin/microsat_MRC_GPU_DLCS_STATS_1024_TPB ${i} > "./microsat_MiraCle_test_results/${n}_microsat_MRC_GPU_DLCS_STATS_1024_TPB.txt"
+    echo "./build/bin/microsat_MRC_GPU_DLCS_STATS_1024_TPB ${i}"
     sleep 10s
 done
